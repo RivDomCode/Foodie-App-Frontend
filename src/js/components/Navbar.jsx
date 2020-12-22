c5e731fc9209df7d95a651e57465a3db849c5819
VirginiaKMartinez <hola@virginiak.es>
2020-12-22 10:33:31 +0000

    subida del navbar con el código del svg sin meter en un componente a parte porque no ha funcionado en las pruebas

--- src/js/components/Navbar.jsx
+++ src/js/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../../styles/navbar.scss";
-import profileIcon from "../../img/profileIcon.svg";
+import navbarProfileIcon from "../svg_components/navbarProfileIcon.js";
 import PropTypes from "prop-types";
 
 const Navbar = () => {
