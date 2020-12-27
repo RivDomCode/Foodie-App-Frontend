9e2745f6bcf68245839f8bb582210541b169537e
riveromj <marigen@gmail.com>
2020-12-27 19:16:14 +0000

    añadiendo funcionalidad a los tabs

--- src/js/components/Tabs.jsx
+++ src/js/components/Tabs.jsx
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../styles/tabs.scss";
+import PropTypes from "prop-types";
 
-const Tabs = () => {
+const Tabs = ({ children }) => {
+	const [activeTab, setActiveTab] = useState(children[0].props.label);
 	return (
 		<div>
 			<ul className="tabs">
-				<li className="current">My Recipes</li>
-				<li>Favorites</li>
+				{children.map(tab => (
+					<li key={tab.props.label} className="current">
+						{children.props.label}
+					</li>
+				))}
 			</ul>
-			<div className="tabs-content" />
+			{children.map(one => (
+				<div key={one.props.label} className="tabs-content">
+					{one.props.children}
+				</div>
+			))}
 		</div>
 	);
 };
 export default Tabs;
+Tabs.propTypes = {
+	children: PropTypes.any,
+	label: PropTypes.any
+};
