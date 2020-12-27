fbbdec6118118e906bbf887e6049c4967a6b62b2
riveromj <marigen@gmail.com>
2020-12-27 09:51:40 +0000

    maquetando tabs

--- src/js/views/profile.js
+++ src/js/views/profile.js
@@ -3,6 +3,7 @@ import "../../styles/profile.scss";
 import User from "../../icons/user.jsx";
 import Edit from "../../icons/edit.jsx";
 import Navbar from "../components/Navbar.jsx";
+import Tabs from "../components/Tabs.jsx";
 
 const Profile = () => {
 	return (
@@ -24,47 +25,7 @@ const Profile = () => {
 					</button>
 				</div>
 				{/* Tabs */}
-				<div className="row tab">
-					<ul className="nav nav-tabs" id="myTab" role="tablist">
-						<li className="nav-item" role="presentation">
-							<a
-								className="nav-link active text-tab"
-								id="recipes-tab"
-								data-bs-toggle="tab"
-								href="#recipes"
-								role="tab"
-								aria-controls="recipes"
-								aria-selected="true">
-								My recipes
-							</a>
-						</li>
-						<li className="nav-item" role="presentation">
-							<a
-								className="nav-link text-tab"
-								id="favorite-tab"
-								data-bs-toggle="tab"
-								href="#favorite"
-								role="tab"
-								aria-controls="favorite"
-								aria-selected="false">
-								Favorite
-							</a>
-						</li>
-					</ul>
-				</div>
-
-				<div className="tab-content" id="myTabContent">
-					<div
-						className="tab-pane fade show active"
-						id="recipes"
-						role="tabpanel"
-						aria-labelledby="recipes-tab">
-						Estoy en mis recetas
-					</div>
-					<div className="tab-pane fade" id="favorite" role="tabpanel" aria-labelledby="favorite-tab">
-						Estoy en vavoritos
-					</div>
-				</div>
+				<Tabs />
 			</div>
 		</div>
 	);
