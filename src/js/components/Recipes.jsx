e3f9d3d55248ebba19aabad2ada5836ed786a6d6
riveromj <marigen@gmail.com>
2020-12-28 11:08:18 +0000

    componentes Recipes y Favorite para dar funcionalidad a Tabs de profile

---
+++ src/js/components/Recipes.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+import "../../styles/tabs.scss";
+
+const Recipes = () => {
+	return (
+		<div className="recipes-tab">
+			<p className="start-message">You have not published any recipe yet.</p>
+		</div>
+	);
+};
+export default Recipes;
