f4b74180675b20fc033812b1bad0c35f9826a3a2
VirginiaKMartinez <hola@virginiak.es>
2020-12-31 09:24:58 +0000

    guardo

--- src/js/components/DropDownSelectCategory.jsx
+++ src/js/components/DropDownSelectCategory.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
 import "../../styles/dropDownSelectCategory.scss";
+import Dropdown from "react-bootstrap/Dropdown";
 
 const DropDownSelectCategory = () => {
 	const [dropdown, setDropdown] = useState("Tag your categorie");
+	const [isChecked, setIsCheched] = useState(false);
 	return (
 		<form className="dropdownCategory">
 			<select
@@ -12,7 +14,24 @@ const DropDownSelectCategory = () => {
 				}}>
 				<option value="Cualquiercosa01">Cualquiercosa01</option>
 				<option value="Cualquiercosa02">Cualquiercosa02</option>
+				<label>Category 01</label>
+				<input
+					type="checkbox"
+					checked={isChecked}
+					onChange={e => {
+						setIsCheched(e.target.checked);
+					}}
+				/>
 			</select>
+
+			<label>Category 01</label>
+			<input
+				type="checkbox"
+				checked={isChecked}
+				onChange={e => {
+					setIsCheched(e.target.checked);
+				}}
+			/>
 		</form>
 	);
 };
