9897f5c22a5367b5d2ee17617cf9944cd1a7904c
VirginiaKMartinez <hola@virginiak.es>
2021-01-02 18:03:07 +0000

    inputs titulo y elaboracion de receta recogen datos en sus respectivos campos

--- src/js/views/newRecipeForm.js
+++ src/js/views/newRecipeForm.js
@@ -26,53 +26,52 @@ const NewRecipeForm = () => {
 		<div>
 			<Navbar />
 			<h2 className="sectionTitle">New recipe</h2>
-			<div className="NoPhotoGreyBackgourd">
-				<div className="noPhoto">
-					<i className="fas fa-image" />
-					<p className="textUploadPhoto">Click here to upload recipe photo</p>
-				</div>
-			</div>
-			<div className="mb-3 recipeTitle">
-				<input
-					onChange={handelChange}
-					onSubmit={handelSubmit}
-					type="text"
-					className="form-control placeholder"
-					name="Recipe title"
-					placeholder="Your recipe title"
-				/>
-			</div>
-
-			<div className="input-group mb-3">
-				<input
-					type="text"
-					className="form-control"
-					placeholder="Add the ingredients here"
-					aria-label="Recipient's username"
-					aria-describedby="button-addon2"
-				/>
-				<button className="buttonAddIngredients" type="button">
-					<div className="addIconDropdown">
-						<i className="fas fa-plus" />
+			<form onChange={handelChange} onSubmit={handelSubmit}>
+				<div className="NoPhotoGreyBackgourd">
+					<div className="noPhoto">
+						<i className="fas fa-image" />
+						<p className="textUploadPhoto">Click here to upload recipe photo</p>
 					</div>
+				</div>
+				<div className="mb-3 recipeTitle">
+					<input
+						type="text"
+						className="form-control placeholder"
+						name="recipeTitle"
+						placeholder="Your recipe title"
+					/>
+				</div>
+
+				<div className="input-group mb-3">
+					<input
+						type="text"
+						className="form-control"
+						placeholder="Add the ingredients here"
+						aria-label="Recipient's username"
+						aria-describedby="button-addon2"
+					/>
+					<button className="buttonAddIngredients" type="button">
+						<div className="addIconDropdown">
+							<i className="fas fa-plus" />
+						</div>
+					</button>
+				</div>
+				<div className="mb-3">
+					<textarea
+						className="textArea"
+						id="exampleFormControlTextarea1"
+						rows="3"
+						placeholder="Elaboration here"
+						name="elaboration"
+					/>
+				</div>
+				<button type="button" className="buttonPublish">
+					<div className="buttonTextPublish">Publish recipe</div>
 				</button>
-			</div>
-			<div className="mb-3">
-				<textarea
-					onChange={handelChange}
-					onSubmit={handelSubmit}
-					className="textArea"
-					id="exampleFormControlTextarea1"
-					rows="3"
-					placeholder="Elaboration here"
-				/>
-			</div>
-			<button type="button" className="buttonPublish">
-				<div className="buttonTextPublish">Publish recipe</div>
-			</button>
-			<button type="button" className="buttonCancel">
-				<div className="buttonTextCancel">Cancel</div>
-			</button>
+				<button type="button" className="buttonCancel">
+					<div className="buttonTextCancel">Cancel</div>
+				</button>
+			</form>
 		</div>
 	);
 };
