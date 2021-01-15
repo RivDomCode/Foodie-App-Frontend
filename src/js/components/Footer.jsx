92835155008543edf7881373ad7c37c184d6c01c
riveromj <marigen@gmail.com>
2021-01-15 23:24:42 +0000

    footer mejorado hacer prueba de responsive

--- src/js/components/Footer.jsx
+++ src/js/components/Footer.jsx
@@ -3,39 +3,49 @@ import "../../styles/footer.scss";
 
 const Footer = () => {
 	return (
-		<footer className="footer row">
-			<div className="col-lg-5 col-md-4  col-xs-12 text-footer">
-				<p className="text-colum ">
-					<span className="subtitle">Final Project for 4Geeks Academy by</span>
-					<span className="names">Marigen Rivero, Pedro Rivas and Virginia Martínez.</span>
-				</p>
-			</div>
-			<div className="col-lg-4 col-md-4 col-xs-12 text-footer">
-				<p className="text-colum">
-					<span className="subtitle">Technologies used </span>
-					HTML, CSS, React and Flux
-				</p>
-			</div>
-			<div className="col-lg-3 col-md-4 col-xs-12 text-footer">
-				<p className="text-colum link">
-					<span className="subtitle">
-						Linked
-						<i className="fab fa-linkedin" /> profile
-					</span>
-					<ul className="developers">
-						<li>
-							<a href="https://www.linkedin.com/in/m-rivero-mirabal">Marigen Rivero Mirabal</a>
-						</li>
-
-						<li>
-							<a href="https://www.linkedin.com/in/pedrorivasdominguez/">Pedro Rivas Domínguez</a>
-						</li>
-						<li>
-							<a href="https://www.linkedin.com/in/virginiakmartinez/"> Virginia K. Martínez</a>
-						</li>
+		<footer className="footer">
+			<div className="containet">
+				<div className="row">
+					<ul className="col-lg-5 col-md-4  col-xs-12 text-footer">
+						<li className="title">Final Project for 4Geeks Academy by</li>
+						<li>Marigen Rivero</li>
+						<li> Pedro Rivas</li>
+						<li>Virginia Martínez</li>
 					</ul>
-				</p>
-				<div />
+					<ul className="col-lg-4 col-md-4 col-xs-12 aling-items-center text-footer">
+						<li className="title">Technologies used</li>
+						<li>HTML5</li>
+						<li>CSS3</li>
+						<li>React</li>
+						<li>Flux</li>
+					</ul>
+					<ul className="col-lg-3 col-md-4 col-xs-12 text-footer">
+						<li className="title">
+							Linked
+							<i className="fab fa-linkedin" /> profile
+						</li>
+						<ul className="text-footer">
+							<li>
+								<a className="linkedin" href="https://www.linkedin.com/in/m-rivero-mirabal">
+									Marigen Rivero Mirabal
+								</a>
+							</li>
+
+							<li>
+								<a className="linkedin" href="https://www.linkedin.com/in/pedrorivasdominguez/">
+									Pedro Rivas Domínguez
+								</a>
+							</li>
+							<li>
+								<a className="linkedin" href="https://www.linkedin.com/in/virginiakmartinez/">
+									{" "}
+									Virginia K. Martínez
+								</a>
+							</li>
+						</ul>
+						<div />
+					</ul>
+				</div>
 			</div>
 		</footer>
 	);
