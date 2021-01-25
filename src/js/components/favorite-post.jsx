df9be4af63e399280b5f6323f24b430aa0c41a26
Ped31 <pedrorivdom@gmail.com>
2021-01-25 18:30:08 +0000

    componente favorite-post creado

---
+++ src/js/components/favorite-post.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import "../../styles/favorite-post.scss";
+
+const FavoritePost = props => {
+	return (
+		<div className="favorite-post">
+			<div className="row no-gutters">
+				<div className="col-md-6 favorite-post-image">
+					<img
+						src="https://cdn.pixabay.com/photo/2017/03/19/18/22/italian-food-2157243_960_720.jpg"
+						className="favorite-post-img"
+						alt="..."
+					/>
+				</div>
+				<div className="col-md-6 right-side">
+					<div className="card-body">
+						<h5 className="favorite-title">Tiramisú</h5>
+					</div>
+					<div className="back-to-edit-profile row">
+						<Link to={"/profile"}>
+							<i className="far fa-edit favorite-edit" />
+						</Link>
+						<i className="fas fa-trash-alt favorite-delete" />
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+FavoritePost.propTypes = {
+	imageUrl: PropTypes.string,
+	recipeTitle: PropTypes.string
+};
+
+export default FavoritePost;
--- src/js/views/comments.js
+++ src/js/views/comments.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import FavoritePost from "../components/favorite-post.jsx";
 
 const Comments = () => {
 	return (
 		<div className="comments">
 			Comentarios: Esta vista tiene que desaparecer, pero de momento la utilizo de pruebas para renderizar los
 			componentes creados
+			<FavoritePost />
 		</div>
 	);
 };
