bddccd058d974c8ecd545508ac03b07a42d5370f
riveromj <marigen@gmail.com>
2021-01-26 20:33:35 +0000

    version1 Conection

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -10,7 +10,24 @@ const getState = ({ getStore, getActions, setStore }) => {
 			favorites: []
 		},
 		actions: {
-			createUser: user => {},
+			createUser: user => {
+				console.log(user);
+				const new_user = {
+					user_name: user.username,
+					email: user.email,
+					password: user.password
+				};
+				fetch("https://3000-eebc3df8-f426-41f7-8f32-d9211915975b.ws-eu03.gitpod.io/" + "user/register", {
+					method: "POST",
+					body: JSON.stringify(new_user),
+					headers: {
+						"Content-Type": "application/json"
+					}
+				})
+					.then(res => res.json())
+					.then(data => console.log(data))
+					.catch(error => console.log(error));
+			},
 
 			addToFavorites: str => {
 				const state = getStore();
