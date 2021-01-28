3681a5a65246a44feb3dd57b7a65f03dbde64829
riveromj <marigen@gmail.com>
2021-01-28 22:48:59 +0000

    Imprimimos el com ponente Card con las recetas de usuario MyRecipes

--- src/js/components/Recipes.jsx
+++ src/js/components/Recipes.jsx
@@ -1,10 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import "../../styles/tabs.scss";
+import { Context } from "../store/appContext";
+import Card from "../components/Card.jsx";
 
 const Recipes = () => {
+	const { store, actions } = useContext(Context);
+	useEffect(() => {
+		actions.getRecipe(true);
+	}, []);
 	return (
 		<div className="recipes-tab">
-			<p className="start-message">You have not published any recipe yet.</p>
+			{store.myRecipes.length == 0 ? (
+				<p className="start-message">You have not published any recipe yet.</p>
+			) : (
+				store.myRecipes.map((recipe, index) => {
+					return <Card recipe={recipe} key={index} />;
+				})
+			)}
 		</div>
 	);
 };
