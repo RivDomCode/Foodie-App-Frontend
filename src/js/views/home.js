a2ceb43faeb5149104d1fbbac4e0244ad2396288
riveromj <marigen@gmail.com>
2021-01-29 09:02:21 +0000

    mensaje si no hay recetas en la aplicacion

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -2,18 +2,17 @@ import React, { useContext } from "react";
 import "../../styles/home.scss";
 import Navbar from "../components/Navbar.jsx";
 import Card from "../components/Card.jsx";
-import PropTypes from "prop-types";
 import { Context } from "../store/appContext";
 
 export const Home = () => {
-	const { store, actions } = useContext(Context);
+	const { store } = useContext(Context);
 	return (
 		<div className="Home">
 			<Navbar />
 			<div className="col-sm-12 col-3">
 				<div className="row first-line d-flex justify-content-sm-center justif">
 					{store.recipes.length == 0 ? (
-						<p>No hay recetas</p>
+						<p className="start-message">Recipes is empty</p>
 					) : (
 						store.recipes.map((recipe, index) => {
 							return <Card recipe={recipe} key={index} />;
